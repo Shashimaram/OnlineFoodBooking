b405512bbc382bc8d9af1b34092e8e3ba614b56a
shashi <shashidherreddy68@gmail.com>
2023-08-21 23:50:41 -0500

    feat:messages on template

--- OnlineFoodBooking/accounts/views.py
+++ OnlineFoodBooking/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import User
 from django.shortcuts import HttpResponse
 from .forms import UserForm
+from django.contrib import messages
 # Create your views here.
 
 # def registerUser(request):
@@ -16,16 +17,28 @@ def registerUser(request):
         
         try:
             if myform.is_valid():
-                user = myform.save(commit=False)
-                user.set_password(request.POST.get('password'))
+                # user = myform.save(commit=False)
+                # user.set_password(request.POST.get('password'))
+                # user.role = User.CUSTOMER
+                # myform.save()
+                # print("form data saved successfully")
+                
+                frist_name = myform.cleaned_data['first_name']
+                last_name = myform.cleaned_data['last_name']
+                username = myform.cleaned_data['username']
+                email = myform.cleaned_data['email']
+                password = myform.cleaned_data['password']
+                user = User.objects.create(first_name=frist_name, last_name=last_name, username=username, email=email, password=password)
                 user.role = User.CUSTOMER
-                myform.save()
-                print("form data saved successfully")
+                user.save()
+                print("user data saved successfully")
+                messages.success(request,"User data saved successfully")
                 return redirect('accounts:registerUser')
             else:
-                print('Some thing went wrong')
                 print("Something went wrong")
                 print(myform.errors)
+                return render(request, template_name='accounts/registration.html', context={'form': myform})
+                
 
         except Exception as e:
             print(f"error{e}")
