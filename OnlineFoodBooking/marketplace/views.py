8b70bfc3423e5c1810733d527085b2b7d752f895
shashi <shashidherreddy68@gmail.com>
2023-09-11 17:43:37 -0500

    added marketpalce button  on home page

--- OnlineFoodBooking/marketplace/views.py
+++ OnlineFoodBooking/marketplace/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from vendor.models import Vendor
 # Create your views here.
 
 def marketplace(request):
-    return render(request, 'marketplace/listings.html')
+    vendor= Vendor.objects.filter(is_approved=True,user__is_active=True)[:8]
+    vendor_count= vendor.count()
+    context ={"vendors":vendor, "vendor_count":vendor_count}
+
+    return render(request, 'marketplace/listings.html',context=context)
