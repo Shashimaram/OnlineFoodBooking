220d78b6603b82cad14f0b893c927559513685ac
shashi <shashidherreddy68@gmail.com>
2023-09-13 19:31:41 -0500

    feat:vendor categorys and fooditems are listed

--- OnlineFoodBooking/marketplace/views.py
+++ OnlineFoodBooking/marketplace/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from vendor.models import Vendor
+from menu.models import Category, Fooditem
 from django.shortcuts import get_object_or_404
+from django.db.models import Prefetch
 # Create your views here.
 
 def marketplace(request):
@@ -12,6 +14,18 @@ def marketplace(request):
 
 def vendor_detail(request,vendor_slug):
     vendor = get_object_or_404(Vendor,vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset=Fooditem.objects.filter(is_available=True)
+        )
+    )
+    fooditem = Fooditem.objects.filter(vendor=vendor)
 
-    context={ "vendor_slug":vendor_slug,"vendor":vendor}
+    context={
+        "vendor_slug":vendor_slug,
+        "vendor":vendor,
+        "categories":categories,
+        "fooditem":fooditem,
+        }
     return render(request, 'marketplace/vendor_details.html', context=context)
